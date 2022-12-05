d530e204cef45f31ed3b6e569699a57f52db84bd
kazido <92008061+kazido@users.noreply.github.com>
2022-12-05 12:04:25 -0500

    drops should be more random during development

--- pyfiles/utils.py
+++ pyfiles/utils.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 from datetime import datetime
+from random import randint
 
 
 def get_project_root() -> Path:
@@ -7,12 +8,12 @@ def get_project_root() -> Path:
 
 
 def seconds_until_tasks():  # Delay drops until half hour
-    minutes = 30
+    minutes = randint(20, 40)
     current_time = datetime.now()
     time_until = minutes - current_time.minute
     if time_until == 0:
         return 0
     elif time_until < 0:
-        minutes = 60
+        minutes = current_time.minute + randint(5, 20)
         time_until = minutes - current_time.minute
     return (time_until * 60) - current_time.second
