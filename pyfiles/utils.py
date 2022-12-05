46ba89fe3e3d4e7efb9e27effcf3797c06a728c6
kazido <92008061+kazido@users.noreply.github.com>
2022-12-05 16:13:39 -0500

    Added switch button to utils

--- pyfiles/utils.py
+++ pyfiles/utils.py
@@ -2,6 +2,9 @@ from pathlib import Path
 from datetime import datetime
 from random import randint
 
+from discord.ui import Button, View
+from discord import Interaction, ButtonStyle
+
 
 def get_project_root() -> Path:
     return Path(__file__).parent.parent
@@ -17,3 +20,17 @@ def seconds_until_tasks():  # Delay drops until half hour
         minutes = current_time.minute + randint(5, 20)
         time_until = minutes - current_time.minute
     return (time_until * 60) - current_time.second
+
+
+class SwitchButton(Button):
+    def __init__(self, parent_interaction: Interaction, view_to_switch_to: View, button_label: str, emoji=None, row=1):
+        self.parent_interaction = parent_interaction
+        self.view_to_switch_to = view_to_switch_to
+        super().__init__(label=button_label, style=ButtonStyle.blurple, emoji=emoji, row=row)
+
+    async def callback(self, switch_interaction: Interaction):
+        if switch_interaction.user != self.parent_interaction.user:
+            return
+        self.view.stop()
+        new_view = self.view_to_switch_to
+        await switch_interaction.response.edit_message(embed=new_view.view_embed, view=new_view)
