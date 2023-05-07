1f3375f62764421e1d608071488101816cd6dbbf
Bryson Ellalasingham <92008061+kazido@users.noreply.github.com>
2023-05-07 21:42:34 +0200

    put crops into module under items

---
+++ bot/data/items/crops/__init__.py
@@ -0,0 +1,28 @@
+import json
+import os
+from pprint import pprint
+
+# Get the directory where this file is located
+dir_path = os.path.dirname(os.path.realpath(__file__))
+
+def get_files_from_path(path: str='.', extension: str=None) -> list:
+    """return list of files from path"""
+    result = {}
+    for subdir, dirs, filenames in os.walk(path):
+        for filename in filenames:
+            filepath = subdir + os.sep + filename
+            if extension == None:
+                result[filename.removesuffix('.json')] = filepath
+            elif filename.lower().endswith(extension.lower()):
+                result[filename.removesuffix('.json')] = filepath
+    return result
+
+filelist = get_files_from_path(path=dir_path, extension='.json')
+
+crops = {}
+
+for identifier, filepath in filelist.items():
+    with open(filepath) as infile:
+        crops[identifier] = (json.load(infile))
+
+
