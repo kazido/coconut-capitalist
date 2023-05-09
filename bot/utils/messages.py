073defd809e3b1cce8219e5b9f6f0afb559f2654
Bryson Ellalasingham <92008061+kazido@users.noreply.github.com>
2023-05-09 16:20:53 +0200

    copying some things from python bot

---
+++ bot/utils/messages.py
@@ -0,0 +1,64 @@
+import random
+from typing import Sequence
+
+import discord
+from discord.ext.commands import Context
+
+from pydis_core.utils import scheduling
+
+import bot
+from bot.constants import MODERATION_ROLES, NEGATIVE_REPLIES
+from logging import getLogger
+
+log = getLogger(__name__)
+
+
+def reaction_check(
+    reaction: discord.Reaction,
+    user: discord.abc.User,
+    *,
+    message_id: int,
+    allowed_emoji: Sequence[str],
+    allowed_users: Sequence[int],
+    allow_mods: bool = True,
+) -> bool:
+    """
+    Check if a reaction's emoji and author are allowed and the message is `message_id`.
+
+    If the user is not allowed, remove the reaction. Ignore reactions made by the bot.
+    If `allow_mods` is True, allow users with moderator roles even if they're not in `allowed_users`.
+    """
+    right_reaction = (
+        user != bot.instance.user
+        and reaction.message.id == message_id
+        and str(reaction.emoji) in allowed_emoji
+    )
+    if not right_reaction:
+        return False
+
+    is_moderator = allow_mods and any(
+        role.id in MODERATION_ROLES for role in getattr(user, "roles", [])
+    )
+
+    if user.id in allowed_users or is_moderator:
+        log.info(f"Allowed reaction {reaction} by {user} on {reaction.message.id}.")
+        return True
+    else:
+        log.info(
+            f"Removing reaction {reaction} by {user} on {reaction.message.id}: disallowed user."
+        )
+        scheduling.create_task(
+            reaction.message.remove_reaction(reaction.emoji, user),
+            suppressed_exceptions=(discord.HTTPException,),
+            name=f"remove_reaction-{reaction}-{reaction.message.id}-{user}",
+        )
+        return False
+    
+async def send_denial(ctx: Context, reason: str) -> discord.Message:
+    """Send an embed denying the user with the given reason."""
+    embed = discord.Embed()
+    embed.colour = discord.Colour.red()
+    embed.title = random.choice(NEGATIVE_REPLIES)
+    embed.description = reason
+
+    return await ctx.send(embed=embed)
