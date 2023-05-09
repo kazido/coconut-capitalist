7940e24662d52c40cdb19c0c0bcec11b94906905
Bryson Ellalasingham <92008061+kazido@users.noreply.github.com>
2023-05-09 16:18:31 +0200

    updating bot structure

--- bot/__init__.py
+++ bot/__init__.py
@@ -1,2 +1,17 @@
-async def setup(bot):
-    pass
+import asyncio
+import os
+import discord
+from typing import TYPE_CHECKING
+
+from pydis_core.utils import apply_monkey_patches
+
+if TYPE_CHECKING:
+    from discord.ext.commands import Bot
+
+# On Windows, the selector event loop is required for aiodns.
+if os.name == "nt":
+    asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+
+apply_monkey_patches()
+
+instance: "Bot" = None  # Global Bot instance.
--- bot/__main__.py
+++ bot/__main__.py
@@ -1,58 +1,64 @@
 import asyncio
 from datetime import datetime
 import os
-import json
+from pprint import pprint
 
 # discord imports
 import discord
 from discord.ext import commands
+from discord import app_commands
+from pydis_core import StartupError
 
 # file imports
-import exts
-import constants
-from utils._extensions import walk_extensions
+import bot
+from bot import exts, constants
+from bot.bot import Bot
+from utils.extensions import walk_extensions
 
-exts_dir_path = exts.__path__[0]
 
-intents = discord.Intents.all()
-intents.message_content = True
-intents.presences = False
-intents.dm_typing = False
-intents.dm_reactions = False
-intents.invites = False
-intents.webhooks = False
-intents.integrations = False
+async def main():
+    intents = discord.Intents.all()
+    intents.message_content = True
+    intents.presences = False
+    intents.dm_typing = False
+    intents.dm_reactions = False
+    intents.invites = False
+    intents.webhooks = False
+    intents.integrations = False
 
-bot = commands.Bot(
-    guild_id=constants.PRIMARY_GUILD,
-    command_prefix=commands.when_mentioned_or(constants.BOT_PREFIX),
-    activity=discord.Game(name=f"Commands: {constants.BOT_PREFIX}help"),
-    case_insensitive=True,
-    max_messages=10_000,
-    allowed_mentions=discord.AllowedMentions(everyone=False),
-    intents=intents,
-    strip_after_prefix=True
-)
-bot.remove_command('help')
+    bot.instance = commands.Bot(
+        guild_id=constants.DiscordGuilds.PRIMARY_GUILD,
+        command_prefix=commands.when_mentioned_or(constants.BOT_PREFIX),
+        activity=discord.Game(name=f"Commands: {constants.BOT_PREFIX}help"),
+        case_insensitive=True,
+        max_messages=10_000,
+        allowed_mentions=discord.AllowedMentions(everyone=False),
+        intents=intents,
+        strip_after_prefix=True
+    )
+    bot.instance.remove_command('help')
+
+    async with bot.instance as _bot:
+        discord.utils.setup_logging()  # 2.1 Logging feature
+        await _bot.start(constants.TOKEN, reconnect=True)
+
+try:
+    asyncio.run(main())
+except StartupError as e:
+    message = "Unknown Startup Error Occurred."
 
 @commands.is_owner()
-@bot.command(hidden=True, aliases=["ch"])
-async def cogs(ctx):
-    for dirpath, _, files in os.walk(exts_dir_path):
-        for file in files:
-            if file.endswith('.py') and file != '__init__.py':
-                dirname = dirpath.split(os.path.sep)[-1]
-                print(dirname)
-                try:
-                    if dirname == 'exts':
-                        ext_path = f"{dirname}.{file[:-3]}"
-                    else:
-                        ext_path = f"exts.{dirname}.{file[:-3]}"
-                    await bot.load_extension(name=ext_path)
-                except commands.ExtensionAlreadyLoaded:
-                    await ctx.send(f"{file[:-3]} is currently loaded.")
-                except commands.ExtensionNotFound:
-                    await ctx.send(f"{file[:-3]} could not be located.")
+@bot.instance.command(hidden=True, aliases=["ch"])
+async def check_cogs(ctx):
+    extensions = walk_extensions(exts)
+    for extension in extensions:
+        print(extension)
+        try:
+            await bot.load_extension(extension)
+        except commands.ExtensionAlreadyLoaded:
+            await ctx.send(f"{extension} is already loaded.")
+        except commands.ExtensionNotFound:
+            await ctx.send(f"{extension} could not be located.")
 
 
 @commands.is_owner()
@@ -68,19 +74,15 @@ async def sync(ctx):
 
 
 @commands.is_owner()
-@bot.command(hidden=True, aliases=['r'])
+@bot.command(hidden=True, aliases=['r', 'rl'])
 async def reload(ctx):
     extensions = []
     select_options = {}
-    for extension in os.listdir(path=exts_dir_path):
-        if extension.endswith('.py'):
-            if extension.startswith('__init__'):
-                pass
-            else:
-                extensions.append(extension)
 
-    for x in extensions:
-        select_options[x] = discord.SelectOption(label=x, value=x)
+    extensions = walk_extensions(exts)
+
+    for extension_name in extensions:
+        select_options[extension_name] = discord.SelectOption(label=extension_name, value=extension_name)
 
     class CogSelect(discord.ui.View):
         def __init__(self, *, timeout=180):
@@ -90,15 +92,12 @@ async def reload(ctx):
         async def selection(self, interaction: discord.Interaction, select: discord.ui.Select):
             if interaction.user != ctx.author:
                 return
-            await bot.reload_extension(f"exts.{select.values[0][:-3]}")
+            await bot.reload_extension(select.values[0])
             await interaction.response.edit_message(
-                content=f"{select.values[0][:-3]} has successfully been reloaded.",
+                content=f"{select.values[0]} has successfully been reloaded.",
                 view=None)
-            await asyncio.sleep(0.6)
-            await ctx.message.delete()
-            await select_message.delete()
 
-    select_message = await ctx.send("Which cog would you like to reload?", view=CogSelect())
+    await ctx.send("Which cog would you like to reload?", view=CogSelect())
 
 
 @commands.is_owner()
@@ -115,37 +114,27 @@ async def unload(ctx, cog):
 
 @commands.is_owner()
 @bot.command(hidden=True)
-async def load(ctx, cog):
+async def load(ctx, extension: str):
+    """
+    Load a bot extension
+    Args:
+        name: The module name to unqualify.
+    Returns:
+        The unqualified module name.
+    """
     try:
-        await bot.load_extension(f"exts.{cog}")
-        await ctx.send(f"{cog} has been successfully loaded.")
+        await bot.load_extension(extension)
+        await ctx.send(f"{extension} has been successfully loaded.")
     except commands.ExtensionAlreadyLoaded:
-        await ctx.send(f"{cog} is already loaded.")
+        await ctx.send(f"{extension} is already loaded.")
     except commands.ExtensionNotFound:
-        await ctx.send(f"{cog} could not be located.")
+        await ctx.send(f"{extension} could not be located.")
 
 
 @bot.event
 async def on_ready():
-    fmt = "%m-%d-%Y %H:%M:%S"  # Put current date into a format and add to bottom of embed
-    print(f"-- BOT RAN --\nRan at: {datetime.strftime(datetime.now(), fmt)}")
-    print("Bot is ready.")
-
-
-async def load_extensions():  # Function for loading cogs upon bot.run
-    # if constants.data['DEBUG']:  # REMOVE THIS LINE
-    #     return
-    for filename in os.listdir(path=exts_dir_path):
-        if filename.endswith('.py') and filename != '__init__.py':
-            await bot.load_extension(f'exts.{filename[:-3]}')
-
-async def main():
-    async with bot:
-        await load_extensions()  # Loads cogs on bot startup
-        discord.utils.setup_logging()  # 2.1 Logging feature
-        # Starts bot using token
-        await bot.start(constants.TOKEN, reconnect=True)
-
+    fmt = "%m-%d-%Y %H:%M:%S"
+    print(f"-- BOT READY --\nRan at: {datetime.strftime(datetime.now(), fmt)}")
 
 if __name__ == '__main__':
     asyncio.run(main())
