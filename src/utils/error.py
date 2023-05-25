d9407d79116001d48fedfb87ee6b554bbfb49d29
kazido <92008061+kazido@users.noreply.github.com>
2023-05-24 20:33:38 -0400

    cleaning up

--- src/utils/error.py
+++ src/utils/error.py
@@ -7,44 +7,20 @@ from discord.ext import commands
 from discord.ext.commands import Cog
 from discord import app_commands
 
+from src.bot import Bot
 
-import datetime
 
+class ErrorHandler(Cog):
+    """Handles errors emitted from commands."""
 
-class CommandErrorHandler(Cog):
-
-    def __init__(self, bot):
+    def __init__(self, bot: Bot):
         self.bot = bot
         self.bot.tree.on_error = self.on_app_command_error
 
-    @Cog.listener()
-    async def on_member_remove(self, member):
-        owner = self.bot.get_user(326903703422500866)
-        channel = await owner.create_dm()
-        await channel.send(f"{member.name} has left the guild and could cause issues in database.\n"
-                           f"Their id: {member.id}")
-
-    @Cog.listener()
-    async def on_member_join(self, member):
-        role_to_add = discord.utils.get(member.guild.roles, name='Unranked')
-        await member.add_roles(role_to_add)
-
-    @Cog.listener()
-    async def on_user_update(self, before, after):
-        pass
-
-    # When a reaction is added to the message in #assign-roles, it adds the user to the database
-    @Cog.listener()
-    async def on_raw_reaction_add(self, payload):
-        if payload.message_id == 966558439880945745:
-            if str(payload.emoji) == "📢":
-                role = discord.utils.get(payload.member.guild.roles, id=966557572544995428)
-                await payload.member.add_roles(role)
-        else:
-            pass
 
     @Cog.listener()
     async def on_app_command_error(self, interaction: discord.Interaction, error: app_commands.AppCommandError):
+        """Listener for handling all slash command related errors"""
         if isinstance(error, app_commands.CommandOnCooldown):
             error_embed = discord.Embed(
                 title=error,
@@ -54,71 +30,24 @@ class CommandErrorHandler(Cog):
             await interaction.delete_original_response()
 
         else:  # If it's a regular error, send the normal traceback
-            print('Ignoring exception in command {}:'.format(interaction.command.qualified_name), file=sys.stderr)
-            traceback.print_exception(type(error), error, error.__traceback__, file=sys.stderr)
+            print('Ignoring exception in slash command {}:'.format(
+                interaction.command.qualified_name), file=sys.stderr)
+            traceback.print_exception(
+                type(error), error, error.__traceback__, file=sys.stderr)
 
     @Cog.listener()
     async def on_command_error(self, ctx: commands.Context, error: commands.CommandError):
+        """Listener for handling all prefix command related errors"""
         if isinstance(error, commands.CommandNotFound):
             return
 
-        elif isinstance(error, commands.DisabledCommand):
-            await ctx.send(f'{ctx.command} has been disabled.')
-
-        elif isinstance(error, commands.MissingRequiredArgument):
-            embed = discord.Embed(
-                title=ctx.command.name,
-                description=ctx.command.description,
-                colour=discord.Color.greyple())
-            embed.add_field(name="How to use this command", value=ctx.command.brief)
-            embed.set_footer(text="For more help, use the -help command")
-            await ctx.send(embed=embed)
-
-        elif isinstance(error, commands.CommandOnCooldown):
-            cool_down = str(datetime.timedelta(seconds=round(error.retry_after)))
-            embed = discord.Embed(title=f"This command is still on cool down: {cool_down}!",
-                                  color=discord.Colour.dark_red())
-            await ctx.send(embed=embed)
-
-        elif isinstance(error, commands.BadUnionArgument):
-            await ctx.send("*Oops!*")
-
-        elif isinstance(error, commands.BadArgument):
-            if ctx.command.qualified_name == 'tag list':
-                await ctx.send('I could not find that member. Please try again.')
-
-        elif isinstance(error, Unregistered):
-            channel = self.bot.get_channel(858552463236923432)
-            await ctx.send(f"You must be registered to use this command. Register in {channel.mention}")
-
-        elif isinstance(error, discord.ext.commands.errors.CheckFailure):
-            print(f"User {ctx.author.name} brought up {error} with the command {ctx.command}.")
-
         else:
-            print('Ignoring exception in command {}:'.format(ctx.command), file=sys.stderr)
-            traceback.print_exception(type(error), error, error.__traceback__, file=sys.stderr)
-
-    @commands.command(name='repeat', aliases=['mimic', 'copy'])
-    async def do_repeat(self, ctx, *, inp: str):
-        """A simple command which repeats your input!
-        Parameters
-        ------------
-        inp: str
-            The input you wish to repeat.
-        """
-        await ctx.send(inp)
-
-    @do_repeat.error
-    async def do_repeat_handler(self, ctx, error):
-        """A local Error Handler for our command do_repeat.
-        This will only listen for errors in do_repeat.
-        The global on_command_error will still be invoked after.
-        """
-
-        if isinstance(error, commands.MissingRequiredArgument):
-            if error.param.name == 'inp':
-                await ctx.send("You forgot to give me input to repeat!")
+            print('Ignoring exception in prefix command {}:'.format(
+                ctx.command), file=sys.stderr)
+            traceback.print_exception(
+                type(error), error, error.__traceback__, file=sys.stderr)
 
 
-async def setup(bot):
-    await bot.add_cog(CommandErrorHandler(bot))
+async def setup(bot: Bot) -> None:
+    """Load the ErrorHandler cog."""
+    await bot.add_cog(ErrorHandler(bot))
