81692ce0cb3f337b688d3f5c0d753ec78b7887b1
kazido <92008061+kazido@users.noreply.github.com>
2023-06-22 23:35:50 -0400

    removed monkey patches

--- src/__init__.py
+++ src/__init__.py
@@ -3,8 +3,6 @@ import os
 import discord
 from typing import TYPE_CHECKING
 
-from pydis_core.utils import apply_monkey_patches
-
 if TYPE_CHECKING:
     from discord.ext.commands import Bot
 
@@ -12,6 +10,5 @@ if TYPE_CHECKING:
 if os.name == "nt":
     asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
 
-apply_monkey_patches()
 
 instance: "Bot" = None  # Global Bot instance.
