c3d32f06499f389d628c94ff88ac1c3fc6eef2e8
kazido <92008061+kazido@users.noreply.github.com>
2023-08-01 03:39:44 -0400

    foraging update

--- src/exts/skills/foraging.py
+++ src/exts/skills/foraging.py
@@ -1,145 +1,180 @@
-from typing import Any
 import discord
-from discord.interactions import Interaction
-import numpy
+import random
 
 from discord import app_commands
 from discord.ext import commands
 from random import randint
 from logging import getLogger
-from src.utils.managers import UserManager, ItemManager, DataManager, ToolManager
-from src.models import DataAreas
-from src.constants import GREEN_CHECK_MARK_URL, RED_X_URL, DiscordGuilds
-from utils.managers import UserManager
+from discord.interactions import Interaction
+from src.utils.managers import (
+    UserManager,
+    DataManager,
+    ToolManager,
+    DataMaster,
+)
+from src.constants import RED_X_URL, DiscordGuilds
+from src.utils.managers import UserManager
+from src.utils.utils import distribute_drops
 
 log = getLogger(__name__)
 log.setLevel(10)
 
-TREE_TITLE = "%s and %s are chopping a **%sft** tree..."
+
+class Tree:
+    # Different possibilities in tree height
+    HEIGHTS = [randint(20, 40), randint(40, 50), randint(50, 60), randint(90, 100)]
+    HEIGHTS_PROBABILITY = [0.499, 0.300, 0.200, 0.001]
+
+    def __init__(self, area_difficulty: int):
+        # Get a random weighted height for the tree
+        base_height = random.choices(self.HEIGHTS, self.HEIGHTS_PROBABILITY)[0]
+        self.height = base_height * area_difficulty
+        self.hitpoints = self.height
+        self.item_pool = DataMaster.select().where(
+            DataMaster.skill == "foraging", DataMaster.drop_rate
+        )
+    
+
+class JoinTreeView(discord.ui.View):
+    def __init__(self, interaction, user: UserManager):
+        super().__init__(timeout=120)
+        self.interaction = interaction
+        self.user = user
+        # Generate a tree based on the user
+        area = DataManager("areas", user.get_field("area_id"))
+        difficulty = area.get_field("difficulty")
+        self.tree = Tree(difficulty)
+        # Add the join button
+        self.add_item(JoinTreeButton())
+
+    async def on_timeout(self) -> None:
+        embed = discord.Embed(
+            title="Nobody else wanted to chop this tree.",
+            description="Maybe a friendly bear will help you! :bear:",
+            color=discord.Color.dark_grey(),
+        )
+        await self.interaction.edit_original_response(embed=embed, view=None)
 
 
-# Defines a custom button that contains logic for 'chopping' a tree
-class ChopButton(discord.ui.Button):
+class JoinTreeButton(discord.ui.Button):
     def __init__(self):
-        super().__init__(style=discord.ButtonStyle.green, label="Chop!")
+        super().__init__(style=discord.ButtonStyle.blurple, label="Join")
 
-    async def callback(self, interaction: discord.Interaction):
-        view: TreeChop = self.view
-
-    @discord.ui.button(label=f"Heave!", style=discord.ButtonStyle.green, disabled=False)
-    async def heave_button(
-        self, heave_interaction: discord.Interaction, button: discord.Button
-    ):
-        if heave_interaction.user.id != tree.user.get_field("user_id"):
+    async def callback(self, join_interaction: Interaction):
+        view: JoinTreeView = self.view
+        if join_interaction.user == view.interaction.user:
             return
-
-        tree.hitpoints -= tree.user_tool.total_power
-        if tree.hitpoints <= 0:
-            await heave_interaction.response.edit_message(embed=tree.embed, view=None)
-        else:
-            chop_embed = discord.Embed(
-                title=f"{tree.user.get_field('name')} and {tree.user_2.get_field('name')} are chopping a **{tree.height}ft** tree...",
-                description=f"*{tree.user_2.get_field('name')} must ho! :arrow_left:*\n**Tree Health: {tree.hitpoints}**",
-                color=0x039410,
+        user_2 = UserManager(join_interaction.user.id, interaction=join_interaction)
+        user_2.tool = ToolManager(user_2.id, user_2.get_field("tool_id", "foraging"))
+        # Ensure that the second user has a tool
+        if user_2.tool.instance is None:
+            embed = discord.Embed(
+                title="You don't have an axe!",
+                description="You need an axe to chop trees. Get one at the shop.",
+                color=discord.Color.red(),
             )
-            self.heave_button.disabled = True
-            self.heave_button.style = discord.ButtonStyle.grey
-            self.ho_button.disabled = False
-            self.ho_button.style = discord.ButtonStyle.green
-            await heave_interaction.response.edit_message(embed=chop_embed, view=self)
-
-    @discord.ui.button(label=f"Ho!", style=discord.ButtonStyle.grey, disabled=True)
-    async def ho_button(
-        self, ho_interaction: discord.Interaction, button: discord.Button
-    ):
-        if ho_interaction.user.id != tree.user_2.get_field("user_id"):
+            await join_interaction.response.send_message(embed=embed, ephemeral=True)
             return
-
-        tree.hitpoints -= tree.user_2_tool.total_power
-        if tree.hitpoints <= 0:
-            await ho_interaction.response.edit_message(embed=tree.embed, view=None)
-        else:
-            chop_embed = discord.Embed(
-                title=f"{tree.user.get_field('name')} and {tree.user_2.get_field('name')} are chopping a **{tree.height}ft** tree...",
-                description=f"*:arrow_right: {tree.user.get_field('name')} must heave!*\n**Tree Health: {tree.hitpoints}**",
-                color=0x039410,
-            )
-            self.ho_button.disabled = True
-            self.ho_button.style = discord.ButtonStyle.grey
-            self.heave_button.disabled = False
-            self.heave_button.style = discord.ButtonStyle.green
-            await ho_interaction.response.edit_message(embed=chop_embed, view=self)
+        new_view = TreeView(view.tree, view.user, user_2, view.interaction)
+        embed = new_view.refresh_embed()
+        await join_interaction.response.edit_message(embed=embed, view=new_view)
+        view.stop()
 
 
 # The view for the tree
-class TreeChop(discord.ui.View):
+class TreeView(discord.ui.View):
     # Initialize view with tree and players
-    def __init__(self, P1, P2, interaction):
+    def __init__(self, tree, p1, p2, interaction):
         super().__init__(timeout=1800)
-        self.tree = Tree()
+        self.tree: Tree = tree
         self.interaction: discord.Interaction = interaction
-        self.current_user = self.P1
+        # Users and area data
+        self.users = [p1, p2]
+        self.current_user = self.users[0]
+        # Add the buttons
+        self.button_1 = ChopButton1()
+        self.button_2 = ChopButton2()
+        self.add_item(self.button_1)
+        self.add_item(self.button_2)
 
     # If the user's don't interact with the tree, disable the view
     async def on_timeout(self):
         expired = discord.Embed(
-            title="The half-chopped tree was left alone...",
-            description="Don't start chopping a tree and forget about it! That's a safety hazard!",
-            color=discord.Color.red(),
+            title="The half-chopped tree was left alone!",
+            description="Don't start chopping a tree and forget about it! \
+                \n:warning: That's a safety hazard!",
+            color=discord.Color.dark_red(),
         )
-        await self.interaction.followup.edit_message(embed=expired, view=None)
+        await self.interaction.edit_original_response(embed=expired, view=None)
 
-
-class Tree:
-    # Different possibilities in tree height
-    heights = [randint(20, 40), randint(40, 50), randint(50, 60), randint(90, 100)]
-    p = [0.499, 0.300, 0.200, 0.001]
-
-    def __init__(self, P1: UserManager, P2: UserManager):
-        # Get a random weighted height for the tree
-        self.height = int(numpy.random.choice(Tree.heights, p=Tree.p))
-        # Get data for the area from player who found tree
-        self.area: DataAreas = DataManager("areas", P1.get_field("area_id"))
-        self.difficulty = self.area.get_field("difficulty")
-        # Adjust the height of the tree according to the difficulty of the area
-        self.height = self.height * self.difficulty
-        self._hitpoints = self.height
-
-    def heave(self):
-        self.hitpoints -= self.user_tool.total_power
-        if self.hitpoints <= 0:
-            return
-        tree_embed = discord.Embed(
-            title=TREE_TITLE % (str(self.user), str(self.user_2), self.height),
-            description=f"*{self.user_2} must ho! :arrow_left:*",
-            color=0xB3EB60,
+    def refresh_embed(self):
+        tree_emoji = "🌳"
+        tree_visual = tree_emoji * int((self.tree.hitpoints / self.tree.height) * 10)
+        embed = discord.Embed(
+            title=f"{self.current_user} is chopping the tree...",
+            description=f"Tree remaining HP: {self.tree.hitpoints}\n{tree_visual}",
+            color=0x039410,
         )
-        tree_embed.add_field(name="Tree Health", value=self.hitpoints)
-        self.heave_button.disabled = True
-        self.heave_button.style = discord.ButtonStyle.grey
-        self.ho_button.disabled = False
-        self.ho_button.style = discord.ButtonStyle.green
+        return embed
 
-    @property
-    def hitpoints(self):
-        return self._hitpoints
-
-    @hitpoints.setter
-    def hitpoints(self, new_hitpoints):
-        if new_hitpoints <= 0:
-            self._hitpoints = 0
-            self.embed = self.on_chopped_down()
+    async def handle_chop(self, interaction: discord.Interaction, next_user):
+        if interaction.user.id != self.current_user.id:
             return
-        self._hitpoints = new_hitpoints
+        self.tree.hitpoints -= self.current_user.tool.total_power
+        if self.tree.hitpoints <= 0:
+            await self.handle_tree_down(interaction)
+            return
+        self.current_user = next_user
+        # Update buttons and embed
+        user_1_turn = self.current_user == self.users[0]
+        user_2_turn = self.current_user == self.users[1]
+        self.button_1.disabled = user_2_turn
+        self.button_2.disabled = user_1_turn
+        self.button_1.style = (
+            discord.ButtonStyle.green if user_1_turn else discord.ButtonStyle.grey
+        )
+        self.button_2.style = (
+            discord.ButtonStyle.green if user_2_turn else discord.ButtonStyle.grey
+        )
+        embed = self.refresh_embed()
+        await interaction.response.edit_message(embed=embed, view=self)
 
-    def on_chopped_down(self):
-        chopped_embed = discord.Embed(
-            title="Tree chopped! :evergreen_tree:",
-            description=f"{self.user.get_field('name')} and {self.user_2.get_field('name')} "
-            f"successfully chopped down a **{self.height}ft** tree!",
+    async def handle_tree_down(self, interaction):
+        embed = discord.Embed(
+            title="TIMBER! :evergreen_tree:",
+            description=f"{str(self.users[0])} and {str(self.users[1])} successfully chopped down a **{self.tree.height}ft** tree!",
             color=0x573A26,
         )
-        return chopped_embed
+        for user in self.users:
+            reward_string = ""
+            drops, bits = distribute_drops(user, self.tree.item_pool, self.tree.height)
+            for item in drops:
+                reward_string += f"**{item['quantity']}x** {str(item['item'])}\n"
+            reward_string += f"**{bits:,}** bits"
+            embed.add_field(name=f"{str(user)}'s Drops", value=reward_string)
+        await interaction.response.edit_message(embed=embed, view=None)
+        self.stop()
+        return
+
+
+# Defines a custom button that contains logic for 'chopping' a tree
+class ChopButton1(discord.ui.Button):
+    def __init__(self):
+        super().__init__(style=discord.ButtonStyle.green, label="Heave!")
+
+    async def callback(self, interaction: discord.Interaction):
+        view: TreeView = self.view
+        await view.handle_chop(interaction, view.users[1])
+
+
+# Defines a custom button for the
+class ChopButton2(discord.ui.Button):
+    def __init__(self):
+        super().__init__(style=discord.ButtonStyle.grey, label="Ho!", disabled=True)
+
+    async def callback(self, interaction: discord.Interaction):
+        view: TreeView = self.view
+        await view.handle_chop(interaction, view.users[0])
 
 
 class ForagingCog(commands.Cog, name="Foraging"):
@@ -150,18 +185,19 @@ class ForagingCog(commands.Cog, name="Foraging"):
 
     # Foraging command group
     primary_guild = DiscordGuilds.PRIMARY_GUILD.value
-    foraging = app_commands.Group(name="foraging", guild_ids=[primary_guild])
+    foraging = app_commands.Group(
+        name="foraging",
+        description="Commands related to the foraging skill.",
+        guild_ids=[primary_guild],
+    )
 
     @foraging.command(name="chop", description="Grab a buddy and chop down a tree.")
     async def chop(self, interaction: discord.Interaction):
         user = UserManager(interaction.user.id, interaction=interaction)
-
-        user_tool = ToolManager(
-            user.get_field("user_id"), user.get_field("tool_id", "foraging")
-        )
+        user.tool = ToolManager(user.id, user.get_field("tool_id", "foraging"))
 
         # If the user does not own an axe
-        if not user_tool.instance:
+        if not user.tool.instance:
             embed = discord.Embed(
                 description="You need an axe to chop trees!", color=discord.Color.red()
             )
@@ -170,71 +206,17 @@ class ForagingCog(commands.Cog, name="Foraging"):
             await interaction.response.send_message(embed=embed)
             return
 
-        # Initialize a tree
-        tree = Tree(user)
-        tree.user_tool = user_tool
-
-        tree.embed = discord.Embed(
-            title=f"Here's a **{tree.height}ft** tree :evergreen_tree:",
+        # Create the view
+        view = JoinTreeView(interaction, user)
+        # Create an embed to attach the view to
+        embed = discord.Embed(
+            title=f"Here's a **{view.tree.height}ft** tree :evergreen_tree:",
             description="Find someone to help you!\nUsers: (1/2)",
             color=0x034509,
-        ).set_footer(text="Hint: try chopping trees in a party")
-
-        class JoinTreeButton(discord.ui.View):
-            async def on_timeout(self) -> None:
-                nobody_joined_embed = discord.Embed(
-                    title="Nobody else wanted to chop this tree.",
-                    description="Maybe a friendly bear will help you! JK.",
-                    color=discord.Color.red(),
-                )
-                await interaction.edit_original_response(
-                    embed=nobody_joined_embed, view=None
-                )
-
-            def __init__(self, *, timeout=1800):
-                super().__init__(timeout=timeout)
-
-            @discord.ui.button(
-                label=f"Join {tree.user.get_field('name')}",
-                style=discord.ButtonStyle.green,
-            )
-            async def join_button(
-                self, join_interaction: discord.Interaction, button: discord.ui.Button
-            ):
-                if join_interaction.user == interaction.user:
-                    return
-                user_2 = UserManager(
-                    join_interaction.user.id, interaction=join_interaction
-                )
-                user_2_tool = ToolManager(
-                    user_2.get_field("user_id"), user_2.get_field("tool_id", "foraging")
-                )
-
-                if user_2_tool.instance is None:
-                    need_axe_embed = discord.Embed(
-                        title="You don't have an axe!",
-                        description="You need an axe to chop trees. Get one at the shop.",
-                        color=discord.Color.red(),
-                    )
-                    await join_interaction.response.send_message(
-                        embed=need_axe_embed, ephemeral=True
-                    )
-                    return
-
-                tree.user_2 = user_2
-                tree.user_2_tool = user_2_tool
-                log.debug(f"User joined with axe power: {user_2_tool.total_power}")
-
-                chop_embed = discord.Embed(
-                    title=f"{tree.user.get_field('name')} and {tree.user_2.get_field('name')} are chopping a **{tree.height}ft** tree.",
-                    description=f"*:arrow_right: {tree.user.get_field('name')} must heave!*\n**Tree Health: {tree.hitpoints}**",
-                    color=0x039410,
-                )
-                await join_interaction.response.edit_message(
-                    embed=chop_embed, view=TreeChop()
-                )
-
-        await interaction.response.send_message(embed=tree_embed, view=JoinTreeButton())
+        )
+        embed.set_footer(text="Hint: try chopping trees in a party")
+        # Send the response to the command
+        await interaction.response.send_message(embed=embed, view=view)
 
 
 async def setup(bot):
--- src/utils/utils.py
+++ src/utils/utils.py
@@ -1,14 +1,63 @@
+import random
+
 from datetime import datetime
-from random import randint
+from src.utils.managers import UserManager, ItemManager, DataManager
 
 
-def seconds_until_tasks():  # Delay drops until half hour
-    minutes = randint(20, 40)
+# Delay drops until half hour
+def seconds_until_tasks():
+    minutes = random.randint(20, 40)
     current_time = datetime.now()
     time_until = minutes - current_time.minute
     if time_until == 0:
         return 0
     elif time_until < 0:
-        minutes = current_time.minute + randint(5, 20)
+        minutes = current_time.minute + random.randint(5, 20)
         time_until = minutes - current_time.minute
     return (time_until * 60) - current_time.second
+
+
+def skewed_random(min_drop, max_drop):
+    """Returns an integer between min_drop and max_drop, skewed towards min_drop"""
+    range_size = max_drop - min_drop + 1
+    skewed_random = min_drop + int((random.random() ** 2) * range_size)
+    return skewed_random
+
+
+def roll_drops(item_pool):
+    """Rolls each item in an item pool and returns all items that rolled and their quantity"""
+    drops = []
+    # Roll each item in the pool once
+    for item in item_pool:
+        item: DataManager = DataManager("master", item.item_id)
+        # If a 1 is rolled, the item was dropped
+        if random.randint(1, item.get_field('drop_rate')) == 1:
+            quantity = skewed_random(item.get_field('min_drop'), item.get_field('max_drop'))
+            drops.append({"item": item, "quantity": quantity})
+    return drops
+
+
+def distribute_drops(user: UserManager, item_pool, bit_multiplier=1):
+    """Distribute bits and drops to a user"""
+    # Generate rewards
+    bits_reward = bit_multiplier * skewed_random(10, 100)
+    roll = roll_drops(item_pool)
+    # Distribute items to user
+    for item in roll:
+        ItemManager.insert_item(user.id, item["item"].get_field(), item["quantity"])
+    # Distribute bits to user
+    user.set_field('purse', user.get_field('purse') + bits_reward)
+    return roll, bits_reward
+
+
+def check_bet(user: UserManager, bet):
+    """Ensures that a user is not betting invalid amounts"""
+    balance = user.get_field("purse")
+    if int(bet) < 0:
+        return f"The oldest trick in the book... Nice try.", False
+    elif int(bet) > balance:
+        return f"No loans. You have {balance} bits.", False
+    elif int(bet) == 0:
+        return "What did you think this was going to do?", False
+    else:
+        return "Passed", True
