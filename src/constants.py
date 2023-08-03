cd29e5c76b79bbaeb3bbec92cc21f38e1aabed94
kazido <92008061+kazido@users.noreply.github.com>
2023-08-03 05:59:47 -0400

    added rarities to constants

--- src/constants.py
+++ src/constants.py
@@ -1,11 +1,9 @@
-import discord
 import os
 import json
 from enum import Enum
 
 
 # Paths
-
 BOT_DIR = os.path.dirname(__file__)
 PROJECT_ROOT = os.path.abspath(os.path.join(BOT_DIR, os.pardir))
 
@@ -15,25 +13,24 @@ with open(config_path, "r") as f:
     data = json.load(f)
 
 
+BOT_PREFIX = "-"
+DATABASE = "livedatabase.db"
+TOKEN = data["primary_token"]
+
 if data["DEV"]:
     BOT_PREFIX = "."
     DATABASE = "testdatabase.db"
     TOKEN = data["secondary_token"]
-else:
-    BOT_PREFIX = "-"
-    DATABASE = "livedatabase.db"
-    TOKEN = data["primary_token"]
 
+
+DEBUG_MODE = False
 if data["DEBUG_MODE"]:
     DEBUG_MODE = True
-else:
-    DEBUG_MODE = False
 
 
+FILE_LOGGING = False
 if data["FILE_LOGGING"]:
     FILE_LOGGING = True
-else:
-    FILE_LOGGING = False
 
 
 class DiscordGuilds(Enum):
@@ -52,18 +49,38 @@ class GamblingChannels(Enum):
 
 class ModerationChannels(Enum):
     DATABASE_LOGS = 858606781093511248
-    
-    
-class PartyRoles(Enum):
-    PARTY_LEADER= 1130004841687699487
 
+
+class PartyRoles(Enum):
+    PARTY_LEADER = 1130004841687699487
+
+class Rarities(Enum):
+    COMMON = (1, 'Common', '0x99F7A7')
+    UNCOMMON = (2, 'Uncommon', '0x63EFFF')
+    RARE = (3, 'Rare', '0x0C61CF')
+    SUPER_RARE = (4, 'Super Rare', '0x6E3ADE')
+    LEGENDARY = (5, 'Legendary', '0xE3AB3B')
+    PREMIUM = (6, 'Premium', '0xE3A019')
+    MYTHICAL = (7, 'Mythical', '0x9F34FF')
+    
+    
+    def __new__(cls, value, name, color):
+        obj = object.__new__(cls)
+        obj._value_ = value
+        obj.rarity_name = name
+        obj.color = color
+        return obj
+    
+    @classmethod
+    def from_value(cls, value):
+        return cls(value)
 
 class CooldownTimes(Enum):
     WORK = 6
     DAILY = 21
     WEEKLY = 167
-    
-    
+
+
 IMAGES_REPO = "https://raw.githubusercontent.com/kazido/images"
 GREEN_CHECK_MARK_URL = f"{IMAGES_REPO}/main/icons/checkmarks/green-checkmark-dist.png"
 RED_X_URL = f"{IMAGES_REPO}/main/icons/checkmarks/red-x.png"
@@ -131,7 +148,7 @@ TOO_RICH_TITLES = [
     "Really?",
     "You have a job, go work it.",
     "No.",
-    "Bet everything, then come back."
+    "Bet everything, then come back.",
 ]
 
 
