8a9261afcf0ddb864b238e20064f1776d18a3f3a
kazido <92008061+kazido@users.noreply.github.com>
2023-08-03 06:00:12 -0400

    optimization

--- src/utils/members.py
+++ src/utils/members.py
@@ -7,13 +7,14 @@ from logging import getLogger
 log = getLogger(__name__)
 
 
-
 def retrieve_rank(user_id, interaction: discord.Interaction):
+    """Retrieve the corresponding rank of a user based on their roles in a Discord guild."""
     guild_roles = interaction.guild.roles
+    user = utils.get(interaction.guild.members, id=user_id)
+    
     for rank in DataRanks.select():
-        user = utils.get(interaction.guild.members, id=user_id)
         discord_role = utils.get(guild_roles, id=rank.rank_id)
-        
+
         # Check to see if the user has any matching role in discord
-        if discord_role in user.roles:    
+        if discord_role in user.roles:
             return rank
