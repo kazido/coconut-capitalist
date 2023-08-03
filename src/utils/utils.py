18b4e165273e600ae8e6a2613b3e03f27651b20e
kazido <92008061+kazido@users.noreply.github.com>
2023-08-03 06:01:27 -0400

    information embeds

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -1,7 +1,13 @@
 import random
+import discord
 
 from datetime import datetime
-from src.utils.managers import UserManager, ItemManager, DataManager
+from src.utils.managers import UserManager, ModelManager, ItemManager
+from src.models import DataMaster, field_formats
+from src.constants import Rarities
+from logging import getLogger
+
+log = getLogger(__name__)
 
 
 # Delay drops until half hour
@@ -27,27 +33,97 @@ def skewed_random(min_drop, max_drop):
 def roll_drops(item_pool):
     """Rolls each item in an item pool and returns all items that rolled and their quantity"""
     drops = []
+    manager = ModelManager(DataMaster)
     # Roll each item in the pool once
     for item in item_pool:
-        item: DataManager = DataManager("master", item.item_id)
+        item: DataMaster
+        item: dict = manager.get(item.item_id)
         # If a 1 is rolled, the item was dropped
-        if random.randint(1, item.get_field('drop_rate')) == 1:
-            quantity = skewed_random(item.get_field('min_drop'), item.get_field('max_drop'))
+        if random.randint(1, item["drop_rate"]) == 1:
+            quantity = skewed_random(item["min_drop"], item["max_drop"])
             drops.append({"item": item, "quantity": quantity})
     return drops
 
 
 def distribute_drops(user: UserManager, item_pool, bit_multiplier=1):
+    # TODO: Fix user.get_field, UserManager has changed
     """Distribute bits and drops to a user"""
     # Generate rewards
     bits_reward = bit_multiplier * skewed_random(10, 100)
     roll = roll_drops(item_pool)
     # Distribute items to user
     for item in roll:
-        ItemManager.insert_item(user.id, item["item"].get_field(), item["quantity"])
+        log.info(f"User rolled {item}")
+        ItemManager.insert_item(user.id, item["item"]["item_id"], item["quantity"])
     # Distribute bits to user
-    user.set_field('purse', user.get_field('purse') + bits_reward)
+    log.info(f"User rolled {bits_reward} bits")
+    user.set_field("purse", user.get_field("purse") + bits_reward)
     return roll, bits_reward
+
+
+def construct_embed(item_id, for_shop: bool):
+    """Constructs an embed filled with formatted data about the item that is passed.
+
+    Args:
+        item_id: The item ID to create an embed for.
+        for_shop (bool): If True, will only add fields needed for the shop.
+
+    Returns:
+        discord.Embed: An embed filled with the items data.
+    """
+    manager = ModelManager(DataMaster)
+    item_data = manager.get_with_related(item_id)
+    log.debug(f"Retrieved {item_data} from manager with item_id: {item_id}")
+
+    # Create an embed with the proper information from the item
+    embed = discord.Embed(
+        title=item_data["display_name"],
+        description=item_data["description"],
+    )
+
+    def format_value(value, field_format: dict, embed: discord.Embed):
+        """Takes an unformatted value and formats it by specific tags"""
+        if field_format.get("get_display_name"):
+            formatted_value = DataMaster.get_display_name(value)
+        elif field_format.get("convert_to_bool"):
+            log.debug("Got convert_to_bool, attempting to convert")
+            formatted_value = "True" if value == 1 else "False"
+            log.debug(f"Result: {formatted_value}")
+        elif field_format.get("rarity"):
+            rarity = Rarities.from_value(value)
+            formatted_value = rarity.rarity_name
+            embed.color = discord.Color.from_str(rarity.color)
+        else:
+            log.debug(f"No fields to be formatted, returning original value: {value}")
+            return value
+        log.debug(f"Finished formatting value: {formatted_value}")
+        return formatted_value
+
+    def construct_stats_string(item_data: dict, field_formats: dict, for_shop: bool):
+        """Constructs a string filled with item information."""
+        stats_string = ""
+        spacing = 10
+        for field_name in field_formats.keys():
+            # For loop will add the fields based on order set in field_formats
+            if field_name in item_data:
+                value = item_data[field_name]
+                field_format = field_formats[field_name]
+                # Update any values based on their field_format
+                value = format_value(value, field_format, embed)
+                if for_shop and not "shop_field" in field_format:
+                    # Only accept shop_fields if for_shop is True
+                    log.debug(f"for_shop == {for_shop} and no shop_field, skipping")
+                    continue
+                # Add the formatted information to the a string
+                if value is not None:
+                    stats_string += field_format["text"].format(value) + "\n"
+
+        return stats_string
+
+    stats_string = construct_stats_string(item_data, field_formats, for_shop)
+    # Add the string to the embed under a field titled "Stats"
+    embed.add_field(name="Stats", value=stats_string)
+    return embed
 
 
 def check_bet(user: UserManager, bet):
