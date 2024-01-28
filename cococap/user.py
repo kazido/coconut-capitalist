75ade4bea4fb7ddd6613c579566a7fdd5bd017d8
Bryson Ellalasingham <92008061+kazido@users.noreply.github.com>
2024-01-28 02:33:35 -0600

    user overhaul

--- cococap/user.py
+++ cococap/user.py
@@ -1,69 +1,46 @@
 import discord
 
 from discord import utils
-from discord.ext import commands
-from cococap.entity_models import *
+
+from cococap import instance
 
 from cococap.item_models import DataRanks
 from cococap.constants import DiscordGuilds
 
+from cococap.models import UserCollection
+
 from logging import getLogger
-
-from cococap import instance
-
 
 log = getLogger(__name__)
 log.setLevel(20)
 
 
-class UserDoesNotExist(Exception):
-    pass
-
-
-class RowDoesNotExist(Exception):
-    pass
-
 class User:
     def __init__(self, uid: int) -> None:
         log.info("Initializing user object with user id: " + str(uid))
         self.uid = uid
-        
-        # set self.field for each field in the users table
-        data, _ = Users.get_or_create(user_id=uid)
-        for field in data.__data__.keys():
-            setattr(self, field, getattr(data, field))
-            
-        # set self.table_name.field for each field in all child tables of the users table
-        for table in UsersChildTables:
-            table_data, _ = table.value.get_or_create(user_id=uid)
-            setattr(self, table.name, table_data)
-            
-        self.update_name()
-            
-    def get_discord_info(self):
+        self.discord_info = self.get_discord_info()
+        self.document: UserCollection
+
+    async def load(self):
+        """Loads the object with information from MongoDB"""
+        self.document = await UserCollection.find_one(
+            UserCollection.discord_id == self.uid
+        )
+        if self.document is None:
+            new_user = UserCollection(name=self.discord_info.name, discord_id=self.uid)
+            await new_user.insert()
+
+    def get_discord_info(self) -> discord.Member:
+        """Gets a user's discord info"""
+        # If I ever expand the bot to other guilds, this needs to chance
         guild: discord.Guild = instance.get_guild(DiscordGuilds.PRIMARY_GUILD.value)
-        self.discord_user: discord.Member = guild.get_member(self.uid)
-        if self.discord_user is None:
-            raise UserDoesNotExist(f"No discord member with ID {self.uid}.")
-        return self.discord_user
-        
-    def update_name(self):
-        user = self.get_discord_info()
-        if self.name != user.name:
-            self.name = user.name
+        discord_user: discord.Member = guild.get_member(self.uid)
+        if discord_user is None:
+            raise Exception(f"No discord member with ID {self.uid}.")
+        return discord_user
 
-    def __str__(self) -> str:
-        return self.name
-        
-    def start_game(self):
-        self.in_game = True
-        log.info(f"Started game for user with ID {self.user_id}")
-
-    def end_game(self):
-        # TODO: give player xp here, maybe rewards?
-        self.in_game = False
-        log.info(f"Ended game for user with ID {self.user_id}")
-
+    # NEEDS UPDATING!!!
     def get_user_rank(self) -> DataRanks:
         """Retrieve the corresponding rank of a user based on their roles in a Discord guild."""
         guild = instance.get_guild(DiscordGuilds.PRIMARY_GUILD.value)
@@ -75,3 +52,40 @@ class User:
             if discord_role in self.discord_user.roles:
                 return rank
         return None
+
+    def __str__(self) -> str:
+        return self.discord_info.name
+
+    # --- UPDATE METHODS ---
+    async def update_purse(self, amount: int):
+        self.document.purse += amount
+        await self.document.save()
+
+    async def update_bank(self, amount: int):
+        self.document.bank += amount
+        await self.document.save()
+
+    async def update_tokens(self, *, tokens: int):
+        self.document.tokens += tokens
+        await self.document.save()
+
+    async def update_game(self, *, in_game: bool):
+        self.document.in_game = in_game
+        await self.document.save()
+
+    async def update_xp(self, *, skill: str, xp: int):
+        if not hasattr(self.document, skill):
+            return "Object does not have skill {skill}."
+        getattr(self.document, skill)["xp"] += xp
+        await self.document.save()
+
+    # --- GET METHODS ---
+    def get_field(self, field: str):
+        if not hasattr(self.document, field):
+            return "Object does not have field {field}."
+        return getattr(self.document, field)
+
+    def get_tool(self, *, skill: str):
+        if not hasattr(self.document, skill):
+            return "Object does not have skill {skill}."
+        return getattr(self.document, skill)["equipped_tool"]
