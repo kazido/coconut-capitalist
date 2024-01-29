4fec7c0d099731134d741107403285e171e2e60c
Bryson Ellalasingham <92008061+kazido@users.noreply.github.com>
2024-01-29 01:49:39 -0600

    formatting

--- cococap/exts/fun/fun.py
+++ cococap/exts/fun/fun.py
@@ -10,15 +10,15 @@ from cococap.bot import Bot
 
 
 @app_commands.guild_only()
-class FunCommands(commands.GroupCog, name='Fun', group_name='fun'):
+class FunCommands(commands.GroupCog, name="Fun", group_name="fun"):
     """Fun commands that spice up the daily chatting."""
 
     def __init__(self, bot: Bot):
         self.bot = bot
-        
+
     @Cog.listener()
     async def on_message(self, message):
-        triggers = ['yey', 'yay', 'woohoo', 'hooray']
+        triggers = ["yey", "yay", "woohoo", "hooray"]
         if message.content.lower() in triggers:
             await message.channel.send(":balloon:")
 
@@ -32,26 +32,29 @@ class FunCommands(commands.GroupCog, name='Fun', group_name='fun'):
 
     @app_commands.command(name="yay", description="YAY!")
     async def yay(self, interaction: Interaction):
-        await interaction.response.send_message(f':balloon:')
+        await interaction.response.send_message(f":balloon:")
 
     @app_commands.command(name="status", description="Change my status. Please be considerate.")
     async def status(self, interaction: Interaction, playing: str):
         self.bot: Bot
         game = discord.Game(playing.replace("playing", ""))
         await self.bot.change_presence(status=discord.Status.online, activity=game)
-        await interaction.response.send_message(f"{self.bot.user.display_name} is now playing: **{playing.replace('playing', '')}**.")
+        await interaction.response.send_message(
+            f"{self.bot.user.display_name} is now playing: **{playing.replace('playing', '')}**."
+        )
 
     @app_commands.command(name="wiki", description="Check the wiki for Terraria!")
     async def wiki(self, interaction: Interaction, item: str | None):
         game_urls = {
             "terraria": "https://terraria.wiki.gg/wiki/",
-            "terrariasearch": "https://terraria.wiki.gg/index.php?search="
+            "terrariasearch": "https://terraria.wiki.gg/index.php?search=",
         }
         if item:
             # Get webpage for searching
             response = requests.get(
-                url=f"{game_urls['terraria' + 'search']}+{item.replace(' ', '+')}")
-            soup = BeautifulSoup(response.content, 'html.parser')
+                url=f"{game_urls['terraria' + 'search']}+{item.replace(' ', '+')}"
+            )
+            soup = BeautifulSoup(response.content, "html.parser")
             # Get title and descriptions for the item
             title = soup.find(id="firstHeading")
             bs = soup.find_all("b")
@@ -60,74 +63,82 @@ class FunCommands(commands.GroupCog, name='Fun', group_name='fun'):
                     embed = discord.Embed(
                         title=f"Could not locate - {item}",
                         description="Make sure you are typing in the **item name** exactly.\n"
-                                    "If it's still not working, contact the **owner** of this bot, "
-                                    "or just do /wiki for the **default wiki page**.",
-                        color=discord.Color.red())
+                        "If it's still not working, contact the **owner** of this bot, "
+                        "or just do /wiki for the **default wiki page**.",
+                        color=discord.Color.red(),
+                    )
                     await interaction.response.send_message(embed=embed)
                     return
             if title.string == "Search results":
                 embed = discord.Embed(
                     title=f"Could not locate - {item}",
                     description="Make sure you are typing in the item name exactly.\n"
-                                "If it's still not working, contact the owner of this bot, "
-                                "or just do /wiki for the default wiki page.",
-                    color=discord.Color.red())
+                    "If it's still not working, contact the owner of this bot, "
+                    "or just do /wiki for the default wiki page.",
+                    color=discord.Color.red(),
+                )
                 await interaction.response.send_message(embed=embed)
                 return
             description = soup.find("div", {"class": "mw-parser-output"})
-            paragraphs = description.select('p')
-            intro = '\n'.join([para.text for para in paragraphs[0:1]])
+            paragraphs = description.select("p")
+            intro = "\n".join([para.text for para in paragraphs[0:1]])
             # Get item sprites
             section_images = soup.find("div", {"class": "section images"})
             embed = discord.Embed(
                 title=title.string,
                 url=f"{game_urls['terraria' + 'search']}+{item.replace(' ', '+')}",
                 description=f"{intro}",
-                color=discord.Color.green()
+                color=discord.Color.green(),
             )
             embed.set_footer(text=f"Requested by {interaction.user.name}")
             try:
                 sprite = section_images.find("img")
-                embed.set_thumbnail(
-                    url=f"https://terraria.wiki.gg{sprite['src']}")
-                embed.set_author(name="Terraria Wiki",
-                                 url=f"{game_urls['terraria']}",
-                                 icon_url="https://terraria.wiki.gg/images/thumb/a/ac/Tree.png/25px-Tree.png")
+                embed.set_thumbnail(url=f"https://terraria.wiki.gg{sprite['src']}")
+                embed.set_author(
+                    name="Terraria Wiki",
+                    url=f"{game_urls['terraria']}",
+                    icon_url="https://terraria.wiki.gg/images/thumb/a/ac/Tree.png/25px-Tree.png",
+                )
             except AttributeError:
                 sprite = None
         else:
             # response = requests.get(url=game_urls[game.name.lower()])  ^^^
             response = requests.get(url=f"{game_urls['terraria']}")
-            soup = BeautifulSoup(response.content, 'html.parser')
+            soup = BeautifulSoup(response.content, "html.parser")
             title = soup.find(id="firstHeading")
             embed = discord.Embed(
-                title=title.string, url=f"{game_urls['terraria']}",
+                title=title.string,
+                url=f"{game_urls['terraria']}",
                 description="**Terraria** is a land of *adventure*! A land of *mystery*! "
-                            "A land that's yours to shape, defend, and enjoy. "
-                            "Your options in **Terraria** are limitless. "
-                            "Are you an action gamer with an itchy trigger finger? A master builder? "
-                            "A collector? An explorer? There's something for everyone.",
-                color=discord.Color.green()
+                "A land that's yours to shape, defend, and enjoy. "
+                "Your options in **Terraria** are limitless. "
+                "Are you an action gamer with an itchy trigger finger? A master builder? "
+                "A collector? An explorer? There's something for everyone.",
+                color=discord.Color.green(),
             )
-            embed.set_footer(
-                text="Use /wiki (item) to see the wiki page about an item!")
+            embed.set_footer(text="Use /wiki (item) to see the wiki page about an item!")
             embed.set_thumbnail(
-                url="https://terraria.wiki.gg/images/thumb/a/ac/Tree.png/25px-Tree.png")
-            embed.set_author(name="Terraria Wiki", url=f"{game_urls['terraria']}",
-                             icon_url="https://terraria.wiki.gg/images/thumb/a/ac/Tree.png/25px-Tree.png")
+                url="https://terraria.wiki.gg/images/thumb/a/ac/Tree.png/25px-Tree.png"
+            )
+            embed.set_author(
+                name="Terraria Wiki",
+                url=f"{game_urls['terraria']}",
+                icon_url="https://terraria.wiki.gg/images/thumb/a/ac/Tree.png/25px-Tree.png",
+            )
         await interaction.response.send_message(embed=embed)
 
     @app_commands.command(name="crafting", description="Check recipes for Terraria items.")
     async def crafting(self, interaction: Interaction, item: str | None):
         response = requests.get(
-            url=f"https://terraria.wiki.gg/index.php?search={item.replace(' ', '+')}")
-        soup = BeautifulSoup(response.content, 'html.parser')
+            url=f"https://terraria.wiki.gg/index.php?search={item.replace(' ', '+')}"
+        )
+        soup = BeautifulSoup(response.content, "html.parser")
         title = soup.find(id="firstHeading")
         section_images = soup.find("div", {"class": "section images"})
         embed = discord.Embed(
             title=title.string,
             url=f"https://terraria.wiki.gg/index.php?search={item.replace(' ', '+')}",
-            color=discord.Color.blue()
+            color=discord.Color.blue(),
         )
         try:
             sprite = section_images.find("img")
@@ -136,12 +147,12 @@ class FunCommands(commands.GroupCog, name='Fun', group_name='fun'):
             sprite = None
         # Get the crafting recipes
         data = []
-        table = soup.find('table', attrs={'class': 'crafts'})
-        table_body = table.find('tbody')
+        table = soup.find("table", attrs={"class": "crafts"})
+        table_body = table.find("tbody")
 
-        rows = table_body.find_all('tr')
+        rows = table_body.find_all("tr")
         for row in rows:
-            cols = row.find_all('td')
+            cols = row.find_all("td")
             cols = [ele.text.strip() for ele in cols]
             data.append([ele for ele in cols if ele])
 
@@ -149,15 +160,13 @@ class FunCommands(commands.GroupCog, name='Fun', group_name='fun'):
         for x in data[2:]:
             dont_add_me = False
             if len(x) == 3:
-                embed.add_field(name="Crafting Station",
-                                value=f"{x[2].replace('or', ' or ')}")
+                embed.add_field(name="Crafting Station", value=f"{x[2].replace('or', ' or ')}")
             for index, strings in enumerate(x):
-                alphabet = 'abcdefghijklmnopqrstuvwxyz'
-                prev = ''
+                alphabet = "abcdefghijklmnopqrstuvwxyz"
+                prev = ""
                 for letter in strings:
-                    if letter.lower() in alphabet and prev == ')':
-                        fixed_string = strings.replace(
-                            f'){letter}', f')\n{letter}')
+                    if letter.lower() in alphabet and prev == ")":
+                        fixed_string = strings.replace(f"){letter}", f")\n{letter}")
                         x.remove(x[index])
                         x.insert(index, fixed_string)
                     prev = letter
