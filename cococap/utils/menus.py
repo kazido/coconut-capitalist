86458ec8c1d514a774de02805d3f10e3c17570de
Bryson Ellalasingham <92008061+kazido@users.noreply.github.com>
2024-02-01 14:14:40 -0600

    menus should be in a working state

--- cococap/utils/menus.py
+++ cococap/utils/menus.py
@@ -2,18 +2,53 @@ import discord
 
 from discord import Interaction, ButtonStyle
 
-
 class Menu(discord.ui.View):
-    def __init__(self, embed=None, prev_menu=None, timeout: float | None = 180):
+    def __init__(self, embed=None, timeout: float | None = 180):
         super().__init__(timeout=timeout)
         self.embed: discord.Embed = embed
-        self.prev_menu: Menu = prev_menu
-        if self.prev_menu is None:
-            self.remove_item(self.back_button)
-        
+
     async def on_timeout(self) -> None:
+        await self.clear_items()
         await self.stop()
         
-    @discord.ui.button(label="Back", emoji="🔙", style=ButtonStyle.gray, row=4)
-    async def back_button(self, interaction: Interaction, button: discord.ui.Button):
-        await interaction.response.edit_message(embed=self.prev_menu.embed, view=self.prev_menu)
+    @discord.ui.button(label="Close", emoji="✖️", style=ButtonStyle.red, row=4)
+    async def back(self, interaction: Interaction, button: discord.ui.Button):
+        self.embed.color = discord.Color.dark_grey()
+        self.embed.set_footer(text=f"Menu has been closed by {interaction.user.name}.")
+        await self.clear_items()
+        await self.stop()
+        await interaction.response.edit_message(embed=self.embed, view=self)
+        
+class ParentMenu(Menu):
+    def __init__(self, embed=None, timeout: float | None = 180):
+        self.menus: list[Menu] = []
+        self.current: int = 0
+        super().__init__(embed, timeout)
+        
+    async def add_submenu(self, submenu: "SubMenu"):
+        self.menus.append(submenu)
+        
+    async def move_forward(self):
+        # Should be called whenever we move forward a menu
+        self.current += 1
+    
+    async def move_backward(self):
+        # Should be called whenever we move back a menu
+        self.current -= 1
+        
+class SubMenu(Menu):
+    # This is essentially our Node class where we store the embed, and the previous
+    def __init__(self, embed=None, timeout: float | None = 180):
+        super().__init__(timeout=timeout)
+        self.embed: discord.Embed = embed
+        
+    @discord.ui.button(label="Back", emoji="🔙", style=ButtonStyle.gray, row=3)
+    async def back(self, interaction: Interaction, button: discord.ui.Button):
+        # Should edit the message and result in the 
+        view: ParentMenu = button.view
+        view.move_backward()
+        prev = view.menus[view.current]
+        await interaction.response.edit_message(embed=prev.embed, view=prev)
+        
+
+
