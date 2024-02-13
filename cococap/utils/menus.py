02ee02081e6770c97fd9271b9ad4ffe2c5334a97
Bryson Ellalasingham <92008061+kazido@users.noreply.github.com>
2024-02-13 04:58:22 -0600

    peak menus

--- cococap/utils/menus.py
+++ cococap/utils/menus.py
@@ -6,10 +6,50 @@ from logging import getLogger
 log = getLogger(__name__)
 log.setLevel(10)
 
+
+class MenuHandler:
+    def __init__(self) -> None:
+        self.menus: list[Menu] = []
+        self.current: int = 0
+        
+    def add_menu(self, menu: "Menu"):
+        self.menus.append(menu)
+        menu.index = len(self.menus) - 1
+        
+    def move_forward(self):
+        # Should be called whenever we move forward a menu
+        log.debug("Menu handler moved forward.")
+        self.current += 1
+    
+    def move_backward(self):
+        # Should be called whenever we move back a menu
+        log.debug("Menu handler moved backward.")
+        self.current -= 1
+        
+    def get_current(self):
+        return self.menus[self.current]
+        
 class Menu(discord.ui.View):
-    def __init__(self, embed=None, timeout: float | None = 180):
-        super().__init__(timeout=timeout)
+    def __init__(self,  handler: MenuHandler, embed=None):
+        super().__init__(timeout=180)
         self.embed: discord.Embed = embed
+        self.button = self.close
+        self.index: int
+        self.handler = handler
+        self.handler.add_menu(self)
+        
+    @property
+    def index(self):
+        """I am the 'index' property."""
+        return self.__index
+    
+    @index.setter
+    def index(self, index: int):
+        self.__index = index
+        if index != 0:
+            self.remove_item(self.close)
+        else:
+            self.remove_item(self.back)
 
     async def on_timeout(self) -> None:
         self.clear_items()
@@ -23,39 +63,11 @@ class Menu(discord.ui.View):
         self.stop()
         await interaction.response.edit_message(embed=self.embed, view=self)
         
-class ParentMenu(Menu):
-    def __init__(self, embed=None, timeout: float | None = 180):
-        self.menus: list[Menu] = []
-        self.current: int = 0
-        super().__init__(embed, timeout)
-        
-    def add_submenu(self, submenu: "SubMenu"):
-        self.menus.append(submenu)
-        
-    def move_forward(self):
-        # Should be called whenever we move forward a menu
-        log.debug("Moved forward.")
-        self.current += 1
-    
-    def move_backward(self):
-        # Should be called whenever we move back a menu
-        log.debug("Moved backward.")
-        self.current -= 1
-        
-class SubMenu(Menu):
-    # This is essentially our Node class where we store the embed, and the previous
-    def __init__(self, embed=None, timeout: float | None = 180):
-        super().__init__(timeout=timeout)
-        self.embed: discord.Embed = embed
-        print(self.children)
-        
-    @discord.ui.button(label="Back", emoji="🔙", style=ButtonStyle.gray, row=3)
+    @discord.ui.button(label = "Back", emoji = "🔙", style = discord.ButtonStyle.gray, row=4)
     async def back(self, interaction: Interaction, button: discord.ui.Button):
-        # Should edit the message and result in the 
-        view: ParentMenu = button.view
-        view.move_backward()
-        prev = view.menus[view.current]
-        await interaction.response.edit_message(embed=prev.embed, view=prev)
+        self.handler.move_backward()
+        menu = self.handler.get_current()
+        await interaction.response.edit_message(embed=menu.embed, view=menu)
         
 
 
