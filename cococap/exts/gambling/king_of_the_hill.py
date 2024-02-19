1db2dc4015ce49e1646814d8923ad7f5d5cb09f5
Bryson Ellalasingham <92008061+kazido@users.noreply.github.com>
2024-02-19 10:40:33 -0600

    started some reward system

--- cococap/exts/gambling/king_of_the_hill.py
+++ cococap/exts/gambling/king_of_the_hill.py
@@ -1,7 +1,10 @@
+import discord
+
 from cococap.constants import DiscordGuilds
 from cococap.bot import Bot
+from cococap.user import User
 
-from discord.ext.commands import Cog
+from discord.ext import commands
 from discord import utils
 
 
@@ -11,18 +14,15 @@ KOTH_CHANNEL_ID = 859262125390168074
 KOTH_ROLE_ID = 895078616063430666
 
 
-# \frac{10000000}{1+e^{-\frac{k}{1.5}\left(x-\left(\frac{168}{2}\right)\right)}}
-
-
-class KingOfTheHill(Cog, name="King of the Hill"):
+class KingOfTheHill(commands.Cog, name="King of the Hill"):
     """Be the last to send a message in the King of the Hill channel to be the King!"""
 
     def __init__(self, bot):
         self.bot = bot
+    
 
-
-    @Cog.listener()
-    async def on_message(self, message):
+    @commands.Cog.listener()
+    async def on_message(self, message: discord.Message):
         if not message.guild:
             return await self.bot.process_commands(message)
 
@@ -30,12 +30,17 @@ class KingOfTheHill(Cog, name="King of the Hill"):
             koth_role = utils.get(message.guild.roles, id=KOTH_ROLE_ID)
             # Retrieve the guild so we can check everyone's roles
             guild = self.bot.get_guild(DiscordGuilds.PRIMARY_GUILD.value)
-            for user in guild.members:
-                if user == message.author:
-                    await user.add_roles(koth_role)
-                else:
-                    if koth_role in user.roles:
-                        await user.remove_roles(koth_role)
+            for member in guild.members:
+                member: discord.Member
+                if member == message.author:
+                    return await member.add_roles(koth_role)
+                if koth_role in member.roles:
+                    await member.remove_roles(koth_role)
+                    # TODO: Make the user get bits based on how long they've been the king!
+                    # user = User(member.id)
+                    # await user.load()
+                    # user.inc_purse(amount=message.created_at - message.channel.last_message)
+                    
 
         if message.channel.id == KOTH_CHANNEL_ID:
             await update_king()
