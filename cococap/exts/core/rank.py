0543adec9daa76c94e8149263693878fad6f4210
Bryson Ellalasingham <92008061+kazido@users.noreply.github.com>
2024-09-03 01:29:26 -0500

    combined /rank and /rankup into one command

--- cococap/exts/core/rank.py
+++ cococap/exts/core/rank.py
@@ -5,7 +5,7 @@ from discord import app_commands
 
 from cococap.user import User
 from cococap.item_models import Ranks
-from cococap.utils.messages import Cembed
+from cococap.utils.messages import Cembed, button_check
 
 
 class RanksCog(commands.Cog):
@@ -23,18 +23,14 @@ class RanksCog(commands.Cog):
         user_rank = await user.get_user_rank()
         await user.load()
 
+        # Displays the user's current rank.
         embed = Cembed(
             title=f"Current Rank: *{user_rank.display_name}* {user_rank.emoji}",
+            desc=f"{user_rank.description}",
             color=discord.Color.from_str("#" + user_rank.color),
             interaction=interaction,
             activity="checking rank",
         )
-        # If the role has permissions, display them
-        # if user_rank:
-        #     perms = ", ".join(ranks[user.rank]["perks"])
-        # else:
-        #     perms = "This rank has no special perks."
-        # embed.add_field(name="Perks", value=perms)
 
         embed.add_field(name="Wage", value=f"{user_rank.wage:,} bits")
         # Finds the next rank in the list and displays the price of the next rank in the embed
@@ -43,8 +39,8 @@ class RanksCog(commands.Cog):
             embed.add_field(
                 name="Next Rank",
                 value=f"{next_rank.display_name} "
-                f"{next_rank.emoji}"
-                f" | Cost: **{next_rank.token_price}** tokens",
+                f"{next_rank.emoji}\n"
+                f"Wage: {next_rank.wage:,} bits",
                 inline=False,
             )
         else:
@@ -53,39 +49,8 @@ class RanksCog(commands.Cog):
                 value="You've reached the max rank! Nice job!",
                 inline=False,
             )
-        embed.set_thumbnail(url=interaction.user.display_avatar)
-        await interaction.response.send_message(embed=embed)
 
-    @app_commands.guilds(856915776345866240, 977351545966432306)
-    @app_commands.command(name="rankup")
-    async def rankup(self, interaction: discord.Interaction):
-        """Spend tokens to move on to the next rank!"""
-        # Load the user
-        user = User(interaction.user.id)
-        user_rank: Ranks = await user.get_user_rank()
-        await user.load()
-
-        if user_rank.next_rank_id:
-            next_rank: Ranks = Ranks.get_by_id(user_rank.next_rank_id)
-
-            rankup_embed = discord.Embed(
-                title=f"{next_rank.display_name} {next_rank.emoji}",
-                description=next_rank.description,
-                color=discord.Color.from_str(f"#{next_rank.color}"),
-            )
-            rankup_embed.add_field(name="Wage", value=f"{next_rank.wage:,} bits")
-            rankup_embed.set_footer(text="You will also receive a new name color.")
-
-        else:
-            embed = discord.Embed(
-                title="Max rank!",
-                description="You are already at the max rank!",
-                color=discord.Color.red(),
-            )
-            await interaction.response.send_message(embed=embed)
-            return
-
-        class RolePurchaseButtons(discord.ui.View):
+        class RolePurchaseButton(discord.ui.View):
             def __init__(self):
                 super().__init__()
                 if user.get_field("tokens") < next_rank.token_price:
@@ -93,10 +58,10 @@ class RanksCog(commands.Cog):
                     self.purchase.disabled = True
 
             @discord.ui.button(
-                label=f"{next_rank.token_price:,} tokens", style=discord.ButtonStyle.green
+                label=f"Rankup: {next_rank.token_price:,}", style=discord.ButtonStyle.green, emoji="🪙"
             )
             async def purchase(self, p_interaction: discord.Interaction, button: discord.ui.Button):
-                if p_interaction.user != interaction.user:
+                if not await button_check(p_interaction, [interaction.user.id]):
                     return
                 role_to_add = discord.utils.get(
                     p_interaction.guild.roles, id=self.next_rank.rank_id
@@ -117,30 +82,8 @@ class RanksCog(commands.Cog):
                 await p_interaction.user.remove_roles(role_to_remove)
                 await user.inc_tokens(tokens=-next_rank.token_price)
 
-            @discord.ui.button(label="Cancel", style=discord.ButtonStyle.red)
-            async def cancel_button(
-                self, cancel_interaction: discord.Interaction, button: discord.ui.Button
-            ):
-                for child in self.children:
-                    child.label = ":("
-                    child.disabled = True
-                if cancel_interaction.user != interaction.user:
-                    return
-                cancel_embed = discord.Embed(
-                    title=f"Cancelled rankup to {next_rank.display_name} " f"{next_rank.emoji}",
-                    description="Unfortunate. Maybe you'll rank up later?",
-                    color=discord.Color.red(),
-                )
-                cancel_embed.add_field(
-                    name="Hypothetical Wage", value=f"{next_rank.wage:,} bits"
-                )
-                cancel_embed.set_footer(text="You WOULD have received a new name color.")
-                await cancel_interaction.response.edit_message(embed=cancel_embed, view=self)
-                await asyncio.sleep(2)
-                await interaction.delete_original_response()
-
-        await interaction.response.send_message(embed=rankup_embed, view=RolePurchaseButtons())
-
+        embed.set_thumbnail(url=interaction.user.display_avatar)
+        await interaction.response.send_message(embed=embed, view=RolePurchaseButton())
 
 async def setup(bot):
     await bot.add_cog(RanksCog(bot))
