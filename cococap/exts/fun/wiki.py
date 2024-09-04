dcf72c34bf60fb24b8ebe514ba320ff640bd3cbd
Bryson Ellalasingham <92008061+kazido@users.noreply.github.com>
2024-09-04 10:44:15 -0500

    import from utils

--- cococap/exts/fun/wiki.py
+++ cococap/exts/fun/wiki.py
@@ -4,6 +4,7 @@ from discord.ext import commands
 from discord import app_commands
 from discord.app_commands import Choice
 
+from cococap.utils.items import construct_embed
 from cococap.item_models import Master
 
 
