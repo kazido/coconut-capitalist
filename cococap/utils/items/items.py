aeb730be9e0e1f665f3c26fcc726c2804b0e007c
Bryson Ellalasingham <92008061+kazido@users.noreply.github.com>
2024-09-04 10:51:43 -0500

    moved field formats to its own file

--- cococap/utils/items/items.py
+++ cococap/utils/items/items.py
@@ -1,5 +1,6 @@
 import discord
 from cococap.constants import Rarities
+from field_formats import field_formats
 
 from cococap.item_models import Master
 
@@ -8,97 +9,6 @@ from logging import getLogger
 
 log = getLogger(__name__)
 log.setLevel(20)
-
-field_formats = {
-    # General fields section
-    "wiki": {
-        "text": "{:}",
-    },
-    "filter_type": {
-        "text": "**Type**: *{:}*",
-    },
-    "rarity": {
-        "text": "**Rarity**: *{:}*",
-        "shop_field": True,
-    },
-    "skill": {
-        "text": "**Category**: *{:}*",
-    },
-    "drop_rate": {
-        "text": "**Drop Rate**: 1/*{:,}*",
-    },
-    "min_drop": {
-        "text": "**Drops at least**: *{:,}*",
-    },
-    "max_drop": {
-        "text": "**Drops at most**: *{:,}*",
-    },
-    # Crop specific section
-    "pet_xp": {
-        "text": "**Pet XP**: *{:,}*",
-        "shop_field": True,
-    },
-    "min_harvest": {
-        "text": "**Min Harvest**: *{:,}*",
-    },
-    "max_harvest": {
-        "text": "**Max Harvest**: *{:,}*",
-    },
-    "grows_from": {
-        "text": "**Grows From**: *{:}*",
-        "shop_field": True,
-        "get_display_name": True,
-    },
-    # Seed specific section
-    "growth_odds": {
-        "text": "**Growth Time**: ~*{:,}* cycles",
-        "shop_field": True,
-    },
-    "grows_into": {
-        "text": "**Grows Into**: *{:}*",
-        "shop_field": True,
-        "get_display_name": True,
-    },
-    # Tool specific section
-    "power": {
-        "text": "**Power**: *{:,}*",
-        "shop_field": True,
-    },
-    # Pet specific section
-    "max_level": {
-        "text": "**Max Level**: *{:,}*",
-        "shop_field": True,
-    },
-    "work_bonus": {
-        "text": "**Work Bonus**: *{:,}* bits",
-        "shop_field": True,
-    },
-    "daily_bonus": {
-        "text": "**Daily Bonus**: *{:,}* tokens",
-        "shop_field": True,
-    },
-    # Rank specific section
-    "token_price": {
-        "text": "**Price**: *{:,}* tokens",
-        "shop_field": True,
-    },
-    "wage": {
-        "text": "**Wage**: *{:,}* bits",
-        "shop_field": True,
-    },
-    "next_rank_id": {
-        "text": "**Next Rank**: *{:}*",
-        "shop_field": True,
-    },
-    # Bottom formatting
-    "price": {
-        "text": "**Price**: *{:,}*",
-        "shop_field": True,
-    },
-    "sell_price": {
-        "text": "**Sell Price**: *{:,}*",
-    },
-}
 
 
 def skewed_roll(min_drop: int, max_drop: int):
