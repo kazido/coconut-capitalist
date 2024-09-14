d64a08796fc8da282bac2ce8533413f0bd57ebf2
Bryson Ellalasingham <92008061+kazido@users.noreply.github.com>
2024-09-14 00:28:48 -0500

    works for now, but needs future proofing!

--- cococap/exts/core/inventory.py
+++ cococap/exts/core/inventory.py
@@ -8,6 +8,7 @@ from cococap.item_models import Master
 from cococap.utils.messages import Cembed, button_check
 from cococap.utils.menus import Menu, MenuHandler, MainMenu
 from cococap.constants import Categories
+from cococap.pagination import LinePaginator
 
 
 class InventoryCog(commands.Cog, name="Inventory"):
@@ -32,6 +33,7 @@ class InventoryCog(commands.Cog, name="Inventory"):
 
         class Inventory(MainMenu):
             def __init__(self, handler: MenuHandler):
+                self.lines = [] # This is to work with pagination, currently not working
                 embed = Cembed(
                     title=f"{interaction.user.name}'s Inventory",
                     desc="",
@@ -39,17 +41,24 @@ class InventoryCog(commands.Cog, name="Inventory"):
                     interaction=interaction,
                     activity="inventory",
                 )
-                #TODO: Handle when inventory gets too big
+                # TODO: Handle when inventory gets too big
                 for k, v in inventory.items():
                     data: Master = Master.get_by_id(k)
-                    embed.description += (
-                        f"{data.emoji} {v['quantity']:,} {data.display_name}\n"
-                    )
+                    # self.lines.append(f"{data.emoji} {v['quantity']:,} {data.display_name}")
+                    embed.description += f"{data.emoji} {v['quantity']:,} {data.display_name}\n"
                 super().__init__(handler, embed=embed)
+                
+            @discord.ui.button(label="Filters", emoji="🔡", style=discord.ButtonStyle.gray, row=3)
+            async def filters(self, interaction: discord.Interaction, button: discord.ui.Button):
+                if not await button_check(interaction, [self.handler.interaction.user.id]):
+                    return
                 self.add_item(CategorySelect(self.handler))
+                self.remove_item(self.filters)
+                await interaction.response.edit_message(view=self)
 
         class SubInventory(Menu):
             def __init__(self, handler: MenuHandler, category: Categories):
+                self.lines = [] # This is to work with pagination, currently not working
                 embed = Cembed(
                     title=f"{interaction.user.name}'s {category.display_name} Inventory {category.emoji}",
                     desc="",
@@ -57,28 +66,26 @@ class InventoryCog(commands.Cog, name="Inventory"):
                     interaction=interaction,
                     activity="inventory",
                 )
-                #TODO: Handle when inventory gets too big
+                # TODO: Handle when inventory gets too big
                 has_item = False
                 for k, v in inventory.items():
                     data: Master = Master.get_by_id(k)
                     if category.display_name.lower() in data.skill:
                         has_item = True
-                        embed.description += (
-                            f"{data.emoji} {v['quantity']:,} {data.display_name}\n"
-                        )
+                        # self.lines.append(f"{data.emoji} {v['quantity']:,} {data.display_name}\n") 
+                        embed.description += f"{data.emoji} {v['quantity']:,} {data.display_name}\n"
                 if not has_item:
                     embed.description = "*You have no items in this category...*"
                 super().__init__(handler, id=category.name.lower(), embed=embed)
-            
-            @discord.ui.button(label = "Back", emoji = "🔙", style = discord.ButtonStyle.gray, row=4)
+
+            @discord.ui.button(label="Back", emoji="🔙", style=discord.ButtonStyle.gray, row=4)
             async def back(self, interaction: discord.Interaction, button: discord.ui.Button):
                 if not await button_check(interaction, [self.handler.interaction.user.id]):
                     return
-                self.handler.move_home()
-                menu = self.handler.get_current()
+                menu = self.handler.move_home()
                 await interaction.response.edit_message(embed=menu.embed, view=menu)
 
-        #TODO: Maybe add a button to choose to filter to hide the ugly select menu by default 
+        # TODO: Maybe add a button to choose to filter to hide the ugly select menu by default
         class CategorySelect(discord.ui.Select):
             def __init__(self, handler: MenuHandler):
                 self.handler = handler
@@ -95,8 +102,7 @@ class InventoryCog(commands.Cog, name="Inventory"):
             async def callback(self, interaction: discord.Interaction):
                 subinventory = SubInventory(self.handler, Categories.from_name(self.values[0]))
                 self.handler.add_menu(subinventory)
-                self.handler.move_to(self.values[0])
-                menu = self.handler.get_current()
+                menu = self.handler.move_to(self.values[0])
                 await interaction.response.edit_message(embed=menu.embed, view=menu)
                 return
 
