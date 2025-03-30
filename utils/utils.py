4f1a98824eefa4bc57f01768c6f2e0332ddb3152
Bryson Ellalasingham <92008061+kazido@users.noreply.github.com>
2025-03-30 02:19:01 -0400

    merged tasks.py with utils.py

--- utils/utils.py
+++ utils/utils.py
@@ -1,3 +1,33 @@
+from datetime import datetime, timezone
+from dateutil import tz
+
+"""
+An example of how to run a loop at a certain time:
+
+Should run at 1:07 pm EST
+time = datetime.time(hour=13, minute=7, tzinfo=est_tz)
+
+@tasks.loop(time=time)
+async def debugging_loop(self):
+    print("Loop ran.")
+"""
+
+est_tz = tz.gettz("US/Michigan")
+
+
+def get_time_until(time: datetime):
+    """Return the difference between now and a datetime.
+
+    Args:
+        time (datetime): The datetime to compare now with.
+
+    Returns:
+        datetime: The time remaining.
+    """
+    c = datetime.now(tz=est_tz)
+    return time - c
+
+
 def _format_time(time: int):
     if len(str(time)) == 1:
         time = "0" + str(time)
