340dafc8ecc251ec46b5c73d3d46764cf5121a0c
Bryson Ellalasingham <92008061+kazido@users.noreply.github.com>
2025-03-30 21:33:38 -0400

    convert is a method now

--- ini/converters/thing_converter.py
+++ ini/converters/thing_converter.py
@@ -1,35 +1,41 @@
 import configparser
 import os
 
-class DynamicObject:
+
+class _DynamicObject:
     def __init__(self, **attributes):
         for key, value in attributes.items():
             setattr(self, key, value)
 
-class INIConverter:
-    @staticmethod
-    def convert(identifier):
-        ini_directory = "ini"
-        try:
-            filename, obj_id = identifier.split(".")
-        except ValueError:
-            raise ValueError("Identifier must be in the format 'filename.identifier'")
+    def __repr__(self):
+        representation = ""
+        for key, value in self.__dict__.items():
+            representation += f"{key}: {value}\n"
+        return representation.strip()
 
-        ini_path = os.path.join(ini_directory, f"{filename}.ini")
-        if not os.path.exists(ini_path):
-            raise FileNotFoundError(f"INI file '{filename}.ini' not found in directory '{ini_directory}'")
 
-        config = configparser.ConfigParser()
-        config.read(ini_path)
+def convert(identifier):
+    ini_directory = "ini"
+    try:
+        filename, obj_id = identifier.split(".")
+    except ValueError:
+        raise ValueError("Identifier must be in the format 'filename.identifier'")
 
-        if obj_id not in config:
-            raise KeyError(f"Identifier '{obj_id}' not found in '{filename}.ini'")
+    ini_path = os.path.join(ini_directory, f"{filename}.ini")
+    if not os.path.exists(ini_path):
+        raise FileNotFoundError(
+            f"INI file '{filename}.ini' not found in directory '{ini_directory}'"
+        )
 
-        attributes = {key: value for key, value in config[obj_id].items()}
-        return DynamicObject(**attributes)
-    
-    
-if __name__ == '__main__':-
-    sword = INIConverter.convert('swords.rustedbroadsword')
-    print(sword.name)
-    print(sword.description)
+    config = configparser.ConfigParser()
+    config.read(ini_path)
+
+    if obj_id not in config:
+        raise KeyError(f"Identifier '{obj_id}' not found in '{filename}.ini'")
+
+    attributes = {key: value for key, value in config[obj_id].items()}
+    return _DynamicObject(**attributes)
+
+
+if __name__ == "__main__":
+    print(convert("swords.rustedbroadsword"))
