cc6f272175f8f034e5d7dee1b888f4433aa78103
Bryson Ellalasingham <92008061+kazido@users.noreply.github.com>
2025-05-08 07:39:13 -0400

    refactor: remove unnecessary constructor and improve user loading in decorators

--- utils/decorators.py
+++ utils/decorators.py
@@ -1,16 +1,34 @@
 from discord import Interaction, app_commands
 from cococap.user import User
+from functools import wraps
+
+from cococap.exts.utils.error import AlreadyInGame
 
 
-class AlreadyInGame(app_commands.CheckFailure):
-    pass
+def load_user(func):
+    @wraps(func)
+    async def wrapper(self, i: Interaction, *args, **kwargs):
+        # Check if the user is already stored in the 'extras' dictionary
+        if "user" not in i.extras:
+            i.extras["user"] = await User(i.user.id).load()  # Store user in 'extras'
+
+        # Call the wrapped function
+        return await func(self, i, *args, **kwargs)
+
+    return wrapper
 
 
 def not_in_game_check():
-    async def predicate(interaction: Interaction):
-        user = await User(interaction.user.id).load()
+    async def predicate(i: Interaction):
+        if "user" not in i.extras:
+            i.extras["user"] = await User(i.user.id).load()
+
+        user = i.extras["user"]
         if user.get_field("in_game"):
-            return False
-        return True  # Pass the check
+            raise AlreadyInGame(
+                "You are already in game somewhere!\n"
+                "Contact admin if you believe this is an error."
+            )
+        return True
 
     return app_commands.check(predicate)
