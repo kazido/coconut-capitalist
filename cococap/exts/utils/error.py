8593a7bcc474eebedc23eee661af85f11e1d9892
Bryson Ellalasingham <92008061+kazido@users.noreply.github.com>
2025-05-08 07:37:10 -0400

    moved error to exts, added custom errors, fixed error handling

---
+++ cococap/exts/utils/error.py
@@ -0,0 +1,51 @@
+import traceback
+import sys
+import discord
+
+from discord import Interaction
+from discord.ext import commands
+from discord.app_commands import AppCommandError, CheckFailure
+
+
+class AlreadyInGame(CheckFailure):
+    pass  # User is trying to play a game while already in one
+
+
+class InvalidBet(CheckFailure):
+    pass  # User makes an invalid bet
+
+
+class ButtonCheckFailure(CheckFailure):
+    pass  # User tries to click button that isn't theirs
+
+
+class ErrorHandler(commands.Cog):
+    """Handles errors emitted from commands."""
+
+    def __init__(self):
+        self.bot.tree.on_error = self.on_app_command_error
+        self.embed = discord.Embed(color=discord.Color.red())
+
+    # APP/SLASH COMMAND ERRORS
+    @commands.Cog.listener()
+    async def on_app_command_error(self, i: Interaction, e: AppCommandError):
+        if isinstance(e, CheckFailure):
+            self.embed.description = e.args[0]
+        else:
+            traceback.print_exception(type(e), e, e.__traceback__, file=sys.stderr)
+            self.embed.description = "Error! Check logs!"
+        return await i.response.send_message(embed=self.embed)
+
+    # REGULAR COMMAND ERRORS
+    @commands.Cog.listener()
+    async def on_command_error(self, ctx: commands.Context, e: commands.CommandError):
+        if isinstance(e, commands.CommandNotFound):
+            self.embed.description = f"I couldn't find that command. Typo?"
+            return await ctx.send(embed=self.embed)
+        else:
+            print("Ignoring exception in [PREFIX] command {}:".format(ctx.command), file=sys.stderr)
+            traceback.print_exception(type(e), e, e.__traceback__, file=sys.stderr)
+
+
+async def setup(bot) -> None:
+    await bot.add_cog(ErrorHandler(bot))
