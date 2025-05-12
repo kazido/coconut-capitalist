a701fdfb6c963473756b1cce1718c550d3575663
Bryson Ellalasingham <92008061+kazido@users.noreply.github.com>
2025-05-11 23:52:37 -0400

    added support for custom errors with titles and footers

--- cococap/exts/utils/error.py
+++ cococap/exts/utils/error.py
@@ -3,20 +3,27 @@ import sys
 
 from discord import Interaction, InteractionResponded
 from discord.ext import commands
-from discord.app_commands import AppCommandError, CheckFailure
+from discord.app_commands import AppCommandError
 from utils.custom_embeds import ErrorEmbed
 
 
-class AlreadyInGame(CheckFailure):
-    pass  # User is trying to play a game while already in one
+class CustomError(AppCommandError):
+    def __init__(self, message, title=None, footer=None):
+        super().__init__(message)
+        self.title = title
+        self.footer = footer
 
 
-class InvalidBet(CheckFailure):
-    pass  # User makes an invalid bet
+class AlreadyInGame(AppCommandError):
+    pass
 
 
-class ButtonCheckFailure(CheckFailure):
-    pass  # User tries to click button that isn't theirs
+class InvalidBet(AppCommandError):
+    pass
+
+
+class ButtonCheckFailure(AppCommandError):
+    pass
 
 
 class ErrorHandler(commands.Cog):
@@ -25,31 +32,34 @@ class ErrorHandler(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.bot.tree.on_error = self.on_app_command_error
-        self.error_embed = ErrorEmbed()
 
     # APP/SLASH COMMAND ERRORS
     @commands.Cog.listener()
     async def on_app_command_error(self, i: Interaction, e: AppCommandError):
-        if isinstance(e, CheckFailure):
-            self.error_embed.description = e.args[0]
-        else:
-            # Send default traceback and have user check logs.
-            traceback.print_exception(type(e), e, e.__traceback__, file=sys.stderr)
+        error_embed = ErrorEmbed()
+        error_embed.description = e.args[0]
+        if hasattr(e, "title") and e.title:
+            error_embed.title = e.title
+        if hasattr(e, "footer") and e.footer:
+            error_embed.set_footer(text=e.footer)
+        # Send default traceback and have user check logs.
+        # traceback.print_exception(type(e), e, e.__traceback__, file=sys.stderr)
         # Attempt to send the message, if it's already been responded to, followup.
         try:
-            return await i.response.send_message(embed=self.error_embed)
+            return await i.response.send_message(embed=error_embed)
         except InteractionResponded:
-            return await i.followup.send(embed=self.error_embed)
+            return await i.followup.send(embed=error_embed)
 
     # REGULAR COMMAND ERRORS
     @commands.Cog.listener()
     async def on_command_error(self, ctx: commands.Context, e: commands.CommandError):
+        error_embed = ErrorEmbed()
         if isinstance(e, commands.CommandNotFound):
-            self.error_embed.description = f"I couldn't find that command. Typo?"
+            error_embed.description = f"I couldn't find that command. Typo?"
         else:
             print("Ignoring exception in [PREFIX] command {}:".format(ctx.command), file=sys.stderr)
             traceback.print_exception(type(e), e, e.__traceback__, file=sys.stderr)
-        return await ctx.send(embed=self.error_embed)
+        return await ctx.send(embed=error_embed)
 
 
 async def setup(bot) -> None:
