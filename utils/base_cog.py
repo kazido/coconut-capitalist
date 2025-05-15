4d38f0da58b041bfa7b1924ba0ffd4cabfc258a5
Bryson Ellalasingham <92008061+kazido@users.noreply.github.com>
2025-05-15 15:10:41 -0400

    refactor: improve interaction_check method with type hinting and enhanced docstring

--- utils/base_cog.py
+++ utils/base_cog.py
@@ -1,10 +1,15 @@
+import discord
 from discord.ext import commands
 from cococap.user import User
 
 
 class BaseCog(commands.Cog):
-    async def interaction_check(self, interaction):
-        """Globally load the user and attach to interaction.extras for all cogs inheriting BaseCog."""
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    async def interaction_check(self, interaction: discord.Interaction):
+        """Globally load the user and attach to interaction.extras for all cogs inheriting BaseCog.
+        This allows us to load a user once and access it anywhere within our command lifecycle."""
         user = await User(interaction.user.id).load()
         interaction.extras.update(user=user)
-        return await super().interaction_check(interaction)
+        return super().interaction_check(interaction)
